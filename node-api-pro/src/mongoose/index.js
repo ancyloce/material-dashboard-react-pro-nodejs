4d3acc36bde9f1d695a4415831c26c68b1c1e03c
ancyloce <ancyloce@gmail.com>
2023-08-21 16:40:51 +0800

    chore: updated database url

--- node-api-pro/src/mongoose/index.js
+++ node-api-pro/src/mongoose/index.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 export const dbConnect = () => {
   mongoose.connection.once("open", () => console.log("DB connection"));
+  mongoose.set("strictQuery", false);
   return mongoose.connect(
-    `mongodb+srv://${process.env.DB_LINK}?retryWrites=true&w=majority`,
+    `mongodb://${process.env.DB_LINK}?retryWrites=true&w=majority`,
     { keepAlive: true }
-  );
+  ).then(() => console.log('DB connected')).catch((err) => console.log(err));
 };
